4f74f84dbdc104a01c0fbd2d0b79256d0869dab9
srijit pokharel <46723334+iamsrijit@users.noreply.github.com>
2024-09-14 21:21:09 +0545

    Update clone_repo.py

--- clone_repo.py
+++ clone_repo.py
@@ -1,1 +1,13 @@
 
+from git import Repo
+import os
+
+# Define the repository URL and directory to clone to
+repo_url = "https://github.com/iamsrijit/Nepse.git"
+repo_dir = os.path.join(os.environ["GITHUB_WORKSPACE"], "nepse_new")
+
+try:
+    # Clone the repository
+    repo = Repo.clone_from(repo_url, repo_dir)
+except Exception as e:
+    print(f"Error cloning repository: {e}")
