874efb2e72b36c1ef18763421116574994d42817
srijit pokharel <46723334+iamsrijit@users.noreply.github.com>
2025-01-31 20:09:37 +0545

    Update nepse_automation.py

--- nepse_automation.py
+++ nepse_automation.py
@@ -130,7 +130,8 @@ headers = {
     #     print(f"File uploaded successfully to {file_path}")
     # else:
     #     print(f"Failed to upload file: {response.status_code}, {response.text}")
- file_path = f'/{file_name}'
+
+file_path = f'/{file_name}'
 
 upload_url = f'https://api.github.com/repos/iamsrijit/Nepse/contents{file_path}'
 
