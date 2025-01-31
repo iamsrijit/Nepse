9ba023666767002500da08d11f650b38526c9861
srijit pokharel <46723334+iamsrijit@users.noreply.github.com>
2025-01-31 21:53:26 +0545

    Update nepse_automation.py

--- nepse_automation.py
+++ nepse_automation.py
@@ -11,7 +11,7 @@ import pandas as pd
 from datetime import datetime
 import pandas as pd
 import numpy as np
-from joblib import Parallel, delayed
+# from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
 
 
